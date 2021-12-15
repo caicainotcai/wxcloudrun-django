7b42b36f7310d8a6d2c9bb3fea52a5af4b0d5dd7
Aanon <96111219+Anon0330@users.noreply.github.com>
2021-12-15 16:42:48 +0800

    Update settings.py

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'NAME': os.environ.get("MYSQL_DATABASE", 'django_demo'),
         'USER': os.environ.get("MYSQL_USERNAME"),
         'HOST': os.environ.get("MYSQL_ADDRESS").split(':')[0],
-        'PORT': os.environ.get("MYSQL_ADDRESS").split(':')[1],
+        'PORT': os.environ.get("MYSQL_ADDRESS").split(':')[-1],
         'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
         'OPTIONS': {'charset': 'utf8mb4'},
     }
